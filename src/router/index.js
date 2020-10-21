11f20e0aaa2b2e88aaadaa5e0388c5a36c9e2a9f
1093872843@qq.com <1093872843@qq.com>
2020-10-21 09:22:22 +0800

    git教学

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,13 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/demo/storeDemo',
+      name: 'storeDemo',
+      //动态导入组件
+      component: () =>
+      import('@/demo/storeDemo'),
     }
   ]
 })
